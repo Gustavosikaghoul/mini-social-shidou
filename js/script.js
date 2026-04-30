202b39db0b57eaa0b8ba575cd7f5d405e9ba4971
𝒢𝒰𝒮𝒯𝒜𝒱𝒪  𝓗𝓔𝓝𝓡𝓘𝓠𝓤𝓔 <gustavo.8925140@aluno.mg.gov.br>
2026-04-30 16:41:01 +0000

    add json

--- js/script.js
+++ js/script.js
@@ -1,8 +1,88 @@
-let likeCount = 0;
+// === "BANCO DE DADOS (JSON Simulado) === 
 
-function curtir() {
-  likeCount++;
-  document.getElementById("likeCount").innerText = likeCount;
+let post = {
+  likeCount: 0,
+  dislikeCount: 0,
+  curtido: false,
+  descurtido: false
 }
 
-document.getElementById("likeBtn").addEventListener("click", curtir);
+// === SERVICE (regras de negócio)
+function curtir() {
+  if (post.curtido == false){
+    post.likeCount++;
+    post.curtido = true;
+    
+
+    if(post.descurtido == true){
+      post.dislikeCount--;
+      post.descurtido = false;
+    }
+
+  }else{
+    post.likeCount--;
+    post.curtido = false;
+  }
+
+}
+
+function descurtir() {
+  if(post.descurtido == false){
+    post.dislikeCount++;
+    post.descurtido = true;
+    
+
+    if(post.curtido == true){
+      post.likeCount--;
+      post.curtido = false;
+    }
+
+  }
+  else{
+    post.dislikeCount--;
+    post.descurtido = false;
+  }
+}
+
+//=== API SIMULADA ==
+
+function getPost(){
+  return post;
+}
+
+function likePost(){
+   curtir ();
+  return post;
+}
+
+function dislikePost(){
+   descurtir ();
+  return post;
+}
+
+//=== VIEWS (interface)===
+
+function atualizarTela(dados){
+  document.getElementById("likeCount").innerText = dados.likeCount;
+  document.getElementById("dislikeCount").innerText = dados.dislikeCount;
+}
+
+//=== CONTROLLER (intermediação evento/regras de negócio)
+
+function clicarCurtir(){
+  let dados = likePost();
+  atualizarTela(dados);
+}
+function clicarDescurtir(){
+  let dados = dislikePost();
+  atualizarTela(dados);
+}
+
+
+// === EVENTOS ====
+document.getElementById("likeBtn").addEventListener("click", clicarCurtir);
+document.getElementById("dislikeBtn").addEventListener("click", clicarDescurtir);
+
+// === INICIALIZAÇÃO DE TELA ===
+
+atualizarTela(getPost());
